b6ad04843efbf4aa780656f911fbfa46d9e84253
David Meade Jr <davidmeadejnr@gmail.com>
2022-12-19 18:59:51 +0000

    Project restructuring.

--- my-algorithms/LastAlgorithm.java
+++ my-algorithms/LastAlgorithm.java
@@ -2,7 +2,12 @@ class LastAlgorithm {
 	public static int[] numbers = {1, 4, 77, 10, 11, 12 , 849, 69, 112}; 
 
 	public static void main(String args[]) {
+		// function that generates an array of n numbers
+		// Get the current time
 		getLastElement(numbers);
+		// Get the current time
+		// Get difference
+      		// print array size time in ms
 	}
 
 	public static int getLastElement(int[] numbers) {
