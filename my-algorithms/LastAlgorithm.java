6aca7eb2eef65bd31e29377c7de8667263341203
David Meade Jr <davidmeadejnr@gmail.com>
2022-12-20 22:07:23 +0000

    Syntax clean-up

--- my-algorithms/LastAlgorithm.java
+++ my-algorithms/LastAlgorithm.java
@@ -1,10 +1,10 @@
 import java.util.LinkedList;
 
-class LastAlgorithm {
-	// public static int[] numbers = {1, 4, 77, 10, 11, 12 , 849, 69, 112}; 
+class LastAlgorithm {	
+  // public static int[] numbers = {1, 4, 77, 10, 11, 12 , 849, 69, 112}; 
 
-	public static void main(String args[]) {
-		// defines the first and last element in the array
+  public static void main(String args[]) {
+    // defines the first and last element in the array
     int firstNum;
     int lastNum;
 
@@ -18,26 +18,26 @@ class LastAlgorithm {
     // Get the current time
     // Get difference
     // print array size time in ms
-	}
-  
+  }
+
   // generates an array of n numbers
   public static LinkedList  generateArray(int firstNum, int lastNum) {
-      LinkedList<Integer> arrayOfNums = new LinkedList<Integer>();
-      for (int i = firstNum; firstNum <= lastNum; i++) {
-        arrayOfNums.add(i);
-        System.out.println(arrayOfNums);
-        return arrayOfNums;
-      }
-
+    LinkedList<Integer> arrayOfNums = new LinkedList<Integer>();
+    for (int i = firstNum; firstNum <= lastNum; i++) {
+      arrayOfNums.add(i);
       System.out.println(arrayOfNums);
       return arrayOfNums;
+    }
+
+    System.out.println(arrayOfNums);
+    return arrayOfNums;
   }
 
   // gets the last element of the array
-	public static int getLastElement(int[] numbers) {
-		int lastNumber = numbers[numbers.length - 1];
+  public static int getLastElement(int[] numbers) {
+    int lastNumber = numbers[numbers.length - 1];
 
-		System.out.println(lastNumber);
-		return lastNumber;
-	}
+    System.out.println(lastNumber);
+    return lastNumber;
+  }
 }
