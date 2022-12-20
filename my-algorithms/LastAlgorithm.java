a0b6083771c57c3855f337e8c92426d9475e1a6d
David Meade Jr <davidmeadejnr@gmail.com>
2022-12-20 21:50:19 +0000

    Method that generates array started.

--- my-algorithms/LastAlgorithm.java
+++ my-algorithms/LastAlgorithm.java
@@ -1,15 +1,39 @@
+import java.util.LinkedList;
+
 class LastAlgorithm {
-	public static int[] numbers = {1, 4, 77, 10, 11, 12 , 849, 69, 112}; 
+	// public static int[] numbers = {1, 4, 77, 10, 11, 12 , 849, 69, 112}; 
 
 	public static void main(String args[]) {
-		// function that generates an array of n numbers
+		// defines the first and last element in the array
+    int firstNum;
+    int lastNum;
+
+    firstNum = 10;
+    lastNum = 100;
+
+    generateArray(firstNum, lastNum); // prints the array
+
 		// Get the current time
-		getLastElement(numbers);
+		// getLastElement(numbers);
 		// Get the current time
 		// Get difference
-      		// print array size time in ms
+    // print array size time in ms
 	}
+  
+  // generates an array of n numbers
+  public static LinkedList  generateArray(int firstNum, int lastNum) {
+      LinkedList<Integer> arrayOfNums = new LinkedList<Integer>();
+      for (int i = firstNum; firstNum <= lastNum; i++) {
+        arrayOfNums.add(i);;
+        System.out.println(arrayOfNums);
+        return arrayOfNums;
+      }
 
+      System.out.println(arrayOfNums);
+      return arrayOfNums;
+  }
+
+  // gets the last element of the arrayi
 	public static int getLastElement(int[] numbers) {
 		int lastNumber = numbers[numbers.length - 1];
 
