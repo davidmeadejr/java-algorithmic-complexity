df991f1ef494bd1cc740f5e063a46e22c918a130
David Meade Jr <davidmeadejnr@gmail.com>
2022-12-21 18:58:44 +0000

    LastAlgorithm using in-built methods created

--- built-in-algorithms/LastAlgorithm.java
+++ built-in-algorithms/LastAlgorithm.java
@@ -1,5 +1,24 @@
-/*
-	TODO:
+/*:
 	Given an array, return the last element
 	using the the .last ruby equivalent in java 
 */
+
+import java.util.ArrayList;
+
+class LastAlgorithm {
+  public static void main(String args[]) {
+     getLastElement();
+  }
+
+  public static int getLastElement() {
+    int n = 7;
+    ArrayList<Integer> numbers = new ArrayList<Integer>(n);
+  
+    for (int i = 0; i <= n; i++) {
+      numbers.add(i);
+    }
+
+    System.out.println(numbers.size() - 1);
+    return numbers.size() - 1;
+  }
+}
