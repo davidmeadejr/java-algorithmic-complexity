762a0e20f7a3c157a41e060ffccd0a99183059be
David Meade Jr <davidmeadejnr@gmail.com>
2022-12-23 19:50:03 +0000

    Added comments

--- built-in-algorithms/ShuffleAlgorithm.java
+++ built-in-algorithms/ShuffleAlgorithm.java
@@ -14,14 +14,14 @@ class ShuffleAlgorithm {
   }
 
   public static ArrayList<Integer> shuffleArray(int n) {
-    ArrayList<Integer> nums = new ArrayList<Integer>(n);
-    Random random = new Random();
+    ArrayList<Integer> nums = new ArrayList<Integer>(n); // generates an array of n numbers
+    Random random = new Random(); 
  
     for (int i = 0; i <= n; i++) {
-      nums.add(random.nextInt(46));
+      nums.add(random.nextInt(46)); // adds a random number between 0 - 46, n number of times
     }
 
-    System.out.println(nums);
+    System.out.println(nums); // prints the shuffled array
     return nums;
   }
 }
