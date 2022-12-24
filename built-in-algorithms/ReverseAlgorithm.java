b6abb50dee1148010c49e8b17a420cc0c5bf62f7
David Meade Jr <davidmeadejnr@gmail.com>
2022-12-24 16:55:20 +0000

    updates

--- built-in-algorithms/ReverseAlgorithm.java
+++ built-in-algorithms/ReverseAlgorithm.java
@@ -4,15 +4,22 @@
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.*;
 
 class ReverseAlgorithm {
   public static void main(String args[]) {
+    long startTime = System.nanoTime();
+
     reverseArray();
+
+    long endTime = System.nanoTime();
+    long duration = (endTime - startTime);
+    System.out.println(duration);
   }
 
   public static ArrayList<Integer> reverseArray() {
     int n;
-    n = 10;
+    n = 50000;
     ArrayList<Integer> numbers = new ArrayList<Integer>(n); // generates and array of n numbers
 
     for (int i = 0; i <= n; i++) {
