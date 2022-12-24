7eebcd1b3a3db0a0ad5b1d83b8bc0eb3f8f03b5b
David Meade Jr <davidmeadejnr@gmail.com>
2022-12-24 17:24:54 +0000

    My own version of the shuffle algorithm created.

--- my-algorithms/ShuffleAlgorithm.java
+++ my-algorithms/ShuffleAlgorithm.java
@@ -1,3 +1,31 @@
 /*
-	TODO
+  Given an array, shuffle the array.
 */
+
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.*;
+
+class ShuffleAlgorithm {
+  public static void main(String args[]) {
+    long startTime = System.nanoTime();
+
+    shuffleArray();
+
+    long endTime = System.nanoTime();
+    long duration = (endTime - startTime);
+    System.out.println(duration);
+  }
+
+  public static ArrayList<Integer> shuffleArray() {
+    ArrayList<Integer> nums = new ArrayList<Integer>(50); // generates an array of n numbers
+    Random random = new Random(); 
+ 
+    for (int i = 0; i <= 10; i++) {
+      nums.add(random.nextInt(100)); // adds a random number between 0 - 46, n number of times
+    }
+
+    System.out.println(nums); // prints the shuffled array
+    return nums;
+  }
+}
