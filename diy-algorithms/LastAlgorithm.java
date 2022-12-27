405588454a6c2c7860e418ad60b4d54f5e206d40
David Meade Jr <davidmeadejnr@gmail.com>
2022-12-27 09:50:44 +0000

    renamed my-algorithms file.

---
+++ diy-algorithms/LastAlgorithm.java
@@ -0,0 +1,47 @@
+import java.util.LinkedList;
+import java.util.Arrays;
+
+class LastAlgorithm {	
+  // public static int[] numbers = {1, 4, 77, 10, 11, 12 , 849, 69, 112}; 
+
+  public static void main(String args[]) {
+    // defines the first and last element in the array
+    int firstNum;
+    int lastNum;
+
+    firstNum = 10;
+    lastNum = 100;
+
+    generateArray(firstNum, lastNum); // prints the array
+    
+
+    // Get the current time
+    // getLastElement(numbers);
+    // Get the current time
+    // Get difference
+    // print array size time in ms
+  }
+
+  // generates an array of n numbers
+  public static LinkedList  generateArray(int firstNum, int lastNum) {
+    LinkedList<Integer> arrayOfNums = new LinkedList<Integer>();
+
+    for (int i = firstNum; firstNum <= lastNum; i++) {
+      arrayOfNums.add(i);
+
+      System.out.println(arrayOfNums);
+      return arrayOfNums;
+    }
+
+    System.out.println(arrayOfNums);
+    return arrayOfNums;
+  }
+
+  // gets the last element of the array
+  public static int getLastElement(int[] numbers) {
+    int lastNumber = numbers[numbers.length - 1];
+
+    System.out.println(lastNumber);
+    return lastNumber;
+  }
+}
---
+++ diy-algorithms/README.md
@@ -0,0 +1,9 @@
+# DIY Algorithms
+
+## Description
+- Visualised the algorithmic complexity of common data structures & algorithms I implmented in Java. With graphs which show how fast a given algorithms runtime is as the size of the input (number of operations) gets larger.
+
+### Shuffle
+- Given an array, shuffle the array.
+- Graph:
+![Built-In Shuffle Algorithm](/graphs/ShuffleAlgorithmDIY.png)
